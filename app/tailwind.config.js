516af861525101800f06a79bec4ba5d889b7de92
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-01-14 03:09:59 +0530

    Update package dependencies to include new Radix UI components and enhance Tailwind CSS configuration. Added @radix-ui/react-accordion, @radix-ui/react-checkbox, and @radix-ui/react-tabs to package.json and package-lock.json. Updated tailwind.config.js to include new animations for accordion components and extended font family options. Integrated tailwind-scrollbar-hide for improved scrollbar styling.

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
   	extend: {
   		fontFamily: {
   			'roboto-mono': [
   				'Roboto Mono"',
+  				'monospace'
+  			],
+  			sans: [
+  				'Cascadia Mono',
   				'monospace'
   			]
   		},
@@ -56,8 +60,30 @@ export default {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			}
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar-hide")],
 };
