3f6be3ad583a6c1865c67280d5a87780afc5887d
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-01-14 02:03:41 +0530

    Add custom hook useSettings for managing animation settings. Implemented state management for duration and easing function selection, enhancing the application's configurability for animations.

---
+++ app/src/components/hooks/useSettings.ts
@@ -0,0 +1,27 @@
+import { useMemo, useState } from "react";
+import { easeFns } from "mayonation";
+
+type Settings = {
+  duration: number;
+  ease?: keyof typeof easeFns;
+};
+
+const useSettings = () => {
+  const [settings, setSettings] = useState<Settings>({
+    duration: 1000,
+    ease: "easeInOut",
+  });
+
+  const easeFnsList = useMemo(
+    () => Object.keys(easeFns) as (keyof typeof easeFns)[],
+    []
+  );
+
+  const handleSettingsChange = (newSettings: typeof settings) => {
+    setSettings(newSettings);
+  };
+
+  return { settings, handleSettingsChange, easeFnsList };
+};
+
+export default useSettings;
